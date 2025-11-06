cce7ebc618b1f91d4c354dff4fcda681b51b99ab
Franciszek <FCzajkowski@proton.me>
2025-11-06 20:48:32 +0100

    Update index.php
    /controller.php to główna strona

--- public/index.php
+++ public/index.php
@@ -1,9 +1,35 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+// Pobranie URL
+$requestUri = $_SERVER['REQUEST_URI'] ?? '/';
+
+// Jeśli użytkownik wchodzi na stronę główną "/"
+if ($requestUri === '/' || $requestUri === '/index.php') {
+    // Wczytanie Twojego starego controller.php
+    require __DIR__ . '/controller.php';
+    exit;
+}
+
+// Dla reszty żądań – standardowy Symfony Kernel
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+
+
+//
+//<?php
+//
+//use App\Kernel;
+//
+//require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+//
+//return function (array $context) {
+//    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+//};
